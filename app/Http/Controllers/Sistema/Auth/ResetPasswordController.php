2e93715c208f97c5c06f2f199bdff16edce091ce
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2017-07-19 19:24:00 -0300

    Correção ResetPasswordController

--- app/Http/Controllers/Sistema/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Sistema/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Sistema\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
 
--- database/seeds/SystemUsersTableSeeder.php
+++ database/seeds/SystemUsersTableSeeder.php
@@ -18,13 +18,5 @@ class SystemUsersTableSeeder extends Seeder
             'password' => '$2y$10$sbyaGhD6rG8MY1g4FArA/Oo9fsOMX/M1CN88cLO4HnUkJ3SgGpnue', 
             'remember_token' => str_random(10),
         ]);
-
-
-        factory(SystemUser::class)->create([
-            'name' => 'Sistema',
-            'email' => 'sistema@test.com',
-            'password' => bcrypt(123456), 
-            'remember_token' => str_random(10),
-        ]);
     }
 }
