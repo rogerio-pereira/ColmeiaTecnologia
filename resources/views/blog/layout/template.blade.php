10721d756f54431ab333cda20becf0dcad8987c5
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-05-10 19:22:42 -0300

    Mautic Tracking

--- resources/views/blog/layout/template.blade.php
+++ resources/views/blog/layout/template.blade.php
@@ -88,4 +88,14 @@
         {!! Html::script('/js/jquery.min.js') !!}
         {!! Html::script('/js/bootstrap.min.js') !!}
     </script>
+
+    {{--Mautic--}}
+    <script>
+        (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+            w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+            m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://marketing.colmeiatecnologia.com.br/mtc.js','mt');
+
+        mt('send', 'pageview');
+    </script>
 </html>
